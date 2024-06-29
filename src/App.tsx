5811fcc233004be15743f29e74c4c877a236000b
Bellerofonty <34703047+Bellerofonty@users.noreply.github.com>
2024-06-29 14:59:09 +0300

    create ButtonGroup using compound components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {ButtonGroup} from './components/ButtonGroup'
 
 function App() {
-  return (
-    <div className="app">
-      <div>Test</div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <ButtonGroup
+                props={{
+                    title: "Title",
+                    counterValue: 3
+                }}
+            >
+                <ButtonGroup.Button/>
+                <ButtonGroup.Counter/>
+                <ButtonGroup.ProgressIndicator/>
+            </ButtonGroup>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -1,1 +1,52 @@
-export {}
+import React, {useContext, createContext, PropsWithChildren} from "react"
+
+type ButtonGroupContext = {
+    props: Props
+}
+
+const ButtonGroupContext = createContext<ButtonGroupContext | undefined>(undefined)
+
+type Props = {
+    title?: string,
+    counterValue?: number,
+}
+
+type ButtonGroupProps = PropsWithChildren & {
+    props: Props
+}
+
+const useButtonGroupContext = () => {
+    const context = useContext(ButtonGroupContext)
+    if (!context) {
+        throw new Error('useButtonGroupContext must be used within ButtonGroup')
+    }
+    return context
+}
+
+export const ButtonGroup = ({ children, props }: ButtonGroupProps) => {
+    return (
+        <ButtonGroupContext.Provider value={{props}}>
+            <div className="button-group">
+                {children}
+            </div>
+        </ButtonGroupContext.Provider>
+    )
+}
+
+const Button = () => {
+    const {props} = useButtonGroupContext()
+    return <button>{props.title}</button>
+}
+ButtonGroup.Button = Button
+
+const Counter = () => {
+    const {props} = useButtonGroupContext()
+    return <div>{props.counterValue}</div>
+}
+ButtonGroup.Counter = Counter
+
+const ProgressIndicator = () => {
+    // const {props} = useButtonGroupContext()
+    return <div>Loading...</div>
+}
+ButtonGroup.ProgressIndicator = ProgressIndicator
